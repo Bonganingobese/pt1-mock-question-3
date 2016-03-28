7d8e97d0a491c1573ab849111307715424e94c6f
bonganingobese <214513896@stu.ukzn.ac.za>
2016-03-28 07:18:56 +0200

    overloaded > and overloaded fuction add to add an int and a fraction object

--- Question3.cpp
+++ Question3.cpp
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //                         COMPUTER METHODS 3 PRACTICAL TEST 1 (MOCK)
-// Name:
-// Student No:
-// Date: 
+// Name:Bongani Ngobese
+// Student No:214513896
+// Date: 28/03/2016
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 
@@ -18,10 +18,55 @@ using namespace std;
 // 3.3 create a driver program to test your overloaded > operators and your overloaded 
 
 class Fraction {
+	friend bool operator >(Fraction &a,Fraction &b)
+	{
+		if (a.num /a.denom >b.num /b.denom)           //check whether fraction ais greater than fraction 2
+		{
+			return true;
+		}
+		return false;
+	};
+	
+
 private:
 	int num;				// numerator;
 	int denom;				// denominator;
 public:
-	Fraction(int n, int d) : num(n), denum(d) { };
-	void print() { cout << num << "/" << denom; };
+	Fraction(int n, int d) : num(n), denom(d) { };
+	void print() { cout << num << "/" << denom<< endl; };
+	Fraction add(int a, Fraction b)
+	{
+		b.num = a*denom + num;
+		b.denom = denom;
+		return b;
+	};
+	Fraction add(Fraction a, int b)
+	{
+		a.num = b*denom + num;                
+
+		a.denom = denom;
+		return a;
+	};
+
 };
+
+int main()
+{
+	Fraction one(1, 2);
+	one = one.add(one, 1);
+	one.print();
+	Fraction two(3, 2);
+	two = two.add(1, two);
+	two.print();
+
+
+	if (one > two)
+	{
+		cout << "Fraction 1 is > than Fraction 2" << endl;
+	}
+	else
+	{
+		cout << "Fraction 2 is > Fraction 1" << endl;
+	}
+
+}
